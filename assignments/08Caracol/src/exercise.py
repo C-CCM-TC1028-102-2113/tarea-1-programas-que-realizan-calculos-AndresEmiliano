fdc3570424cc4e15f6ee85418dc074e9de0cee4c
AndresEmiliano <89090646+AndresEmiliano@users.noreply.github.com>
2021-08-23 22:24:04 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+a = float(input("tiempo que el caracol recorrera:"))
+b = a*60
+c = b*.57
+print("los centimetros recorridos son:")
+print(c)
+
 
 if __name__ == '__main__':
     main()
